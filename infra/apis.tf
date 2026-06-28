321426653108886054786d1ea31b1f701caf65d6
ALucek <adamrl2000@gmail.com>
2026-06-27 23:37:05 -0400

    feat(infra): enable the GCP APIs

---
+++ infra/apis.tf
@@ -0,0 +1,17 @@
+locals {
+  gcp_apis = [
+    "run.googleapis.com",
+    "sqladmin.googleapis.com",
+    "secretmanager.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "enabled" {
+  for_each           = toset(local.gcp_apis)
+  service            = each.value
+  disable_on_destroy = false
+}
